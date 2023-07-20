558a3ce70083805f2428e1e3231bd74c3860bb82
sabrallah <sabrallah1991@gmail.com>
2023-07-20 15:27:42 -0700

    task3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,6 +1,43 @@
 #include "sort.h"
 
 /**
+ * swap - swaps two integers in an array
+ * @array: array containing the integers
+ * @i: index of the first integer
+ * @j: index of the second integer
+ */
+void swap(int *array, int i, int j)
+{
+	int temp = array[i];
+
+	array[i] = array[j];
+	array[j] = temp;
+}
+
+/**
+ * median_of_three - chooses the median of the first, middle, and last elements
+ * of a partition as the pivot
+ * @array: array containing the partition
+ * @low: lowest index of the partition
+ * @high: highest index of the partition
+ *
+ * Return: index of the pivot
+ */
+int median_of_three(int *array, int low, int high)
+{
+	int mid = (low + high) / 2;
+
+	if (array[low] > array[mid])
+		swap(array, low, mid);
+	if (array[low] > array[high])
+		swap(array, low, high);
+	if (array[mid] > array[high])
+		swap(array, mid, high);
+
+	return (mid);
+}
+
+/**
  * lomuto_partition - partitions an array using the Lomuto partition scheme
  * @array: array to partition
  * @low: lowest index of the partition
@@ -11,25 +48,25 @@
  */
 int lomuto_partition(int *array, int low, int high, size_t size)
 {
-	int pivot = array[high];
+	int pivot_index = median_of_three(array, low, high);
+	int pivot = array[pivot_index];
 	int i = low;
-	int j, temp;
+	int j;
+
+	swap(array, pivot_index, high);
+	print_array(array, size);
 
 	for (j = low; j < high; j++)
 	{
 		if (array[j] <= pivot)
 		{
-			temp = array[i];
-			array[i] = array[j];
-			array[j] = temp;
+			swap(array, i, j);
 			if (i != j)
 				print_array(array, size);
 			i++;
 		}
 	}
-	temp = array[i];
-	array[i] = array[high];
-	array[high] = temp;
+	swap(array, i, high);
 	if (i != j)
 		print_array(array, size);
 
@@ -57,7 +94,8 @@ void quick_sort_helper(int *array, int low, int high, size_t size)
 
 /**
  * quick_sort - sorts an array of integers in ascending order using the Quick
- * sort algorithm with the Lomuto partition scheme
+ * sort algorithm with the Lomuto partition scheme and median-of-three method
+ * for choosing the pivot
  * @array: array to sort
  * @size: size of the array
  */
