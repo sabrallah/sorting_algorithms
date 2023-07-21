1592f7372aaf21b2a2e0b4b8b57c48ebf6fef9f1
sabrallah <sabrallah1991@gmail.com>
2023-07-21 06:35:47 -0700

    task6

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -12,38 +12,44 @@
  * using the Counting sort algorithm. It assumes that the input array will
  * contain only numbers >= 0. The function prints the counting array once it
  * is set up. The counting array is of size k + 1 where k is the largest number
- * in the input array.
+ * in the input array. This version of the function takes into account the
+ * minimum value in the input array to reduce the size of the counting array
+ * and potentially improve the efficiency of the algorithm.
  */
 void counting_sort(int *array, size_t size)
 {
-	int i, j, max = 0;
+	int i, j, max = 0, min = array[0];
 	int *count, *output;
 
 	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < (int)size; i++)
+	{
 		if (array[i] > max)
 			max = array[i];
+		if (array[i] < min)
+			min = array[i];
+	}
 
-	count = malloc(sizeof(int) * (max + 1));
+	count = malloc(sizeof(int) * (max - min + 1));
 	output = malloc(sizeof(int) * size);
 
-	for (i = 0; i <= max; i++)
+	for (i = 0; i <= max - min; i++)
 		count[i] = 0;
 
 	for (i = 0; i < (int)size; i++)
-		count[array[i]] += 1;
+		count[array[i] - min] += 1;
 
-	for (i = 1; i <= max; i++)
+	for (i = 1; i <= max - min; i++)
 		count[i] += count[i - 1];
 
-	print_array(count, max + 1);
+	print_array(count, max - min + 1);
 
 	for (j = size - 1; j >= 0; j--)
 	{
-		output[count[array[j]] - 1] = array[j];
-		count[array[j]] -= 1;
+		output[count[array[j] - min] - 1] = array[j];
+		count[array[j] - min] -= 1;
 	}
 
 	for (i = 0; i < (int)size; i++)
