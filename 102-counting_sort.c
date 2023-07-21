53521e06d111fb25b55be49f140d7eac5802474d
sabrallah <sabrallah1991@gmail.com>
2023-07-21 09:43:49 -0700

    task6

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,96 +1,93 @@
 #include "sort.h"
 
-
 /**
- * start_initi - init array 0
+ * my_arr_init - init by 0 array
  *
- * @array: should init array
- * @size: array size
+ * @array: init be initialased
+ * @size: size array
  */
-void start_initi(int *array, int size)
+void my_arr_init(int *array, int size)
 {
-	int sy;
+	int sa;
 
-	for (sy = 0; sy < size; sy++)
-		array[sy] = 0;
+	for (sa = 0; sa < size; sa++)
+		array[sa] = 0;
 }
 
-
 /**
- * avoir_maxi - avoir maximaum valeur
+ * my_getmax - get max value in array
  *
- * @array: plafond value from of array
- * @size: array size
+ * @array: array my_max value from
+ * @size: size the array
  *
- * Return: array max value
+ * Return: max value in array
  */
-int avoir_maxi(int *array, size_t size)
+int my_getmax(int *array, size_t size)
 {
-	int plafond;
-	size_t sy;
+	int my_max;
+	size_t sa;
 
 	if (size < 2)
 		return (0);
 
-	plafond = array[0];
+	my_max = array[0];
 
-	for (sy = 1; sy < size; sy++)
+	for (sa = 1; sa < size; sa++)
 	{
-		if (plafond < array[sy])
-			plafond = array[sy];
+		if (my_max < array[sa])
+			my_max = array[sa];
 	}
 
-	return (plafond);
+	return (my_max);
 }
 
-
 /**
- * counting_sort - sorts array of counting
- * sort algo
+ * counting_sort - sort array counting
+ * sor algo
  *
  * @array: init arrays
- * @size: the array size
+ * @size: size arrays
  */
 void counting_sort(int *array, size_t size)
 {
-	int y = avoir_maxi(array, size), s, *place, *addplace, *my_sorted;
-	size_t sy;
+	int y = my_getmax(array, size), j, *my_position, *my_sumposition, *my_sorted;
+	size_t sa;
 
 	if (y == 0)
 		return;
 
-	place = malloc(sizeof(int) * (y + 1));
-	if (!place)
+	my_position = malloc(sizeof(int) * (y + 1));
+	if (!my_position)
 		return;
-	start_initi(place, y + 1);
+	my_arr_init(my_position, y + 1);
 
-	for (sy = 0; sy < size; sy++)
-		place[array[sy]] += 1;
+	for (sa = 0; sa < size; sa++)
+		my_position[array[sa]] += 1;
 
-	addplace = malloc(sizeof(int) * (y + 1));
-	if (!addplace)
+	my_sumposition = malloc(sizeof(int) * (y + 1));
+	if (!my_sumposition)
 		return;
-	start_initi(addplace, y + 1);
-	addplace[0] = place[0];
+	my_arr_init(my_sumposition, y + 1);
+	my_sumposition[0] = my_position[0];
 
-	for (s = 1; s < (y + 1); s++)
-		addplace[s] = place[s] + addplace[s - 1];
+	for (j = 1; j < (y + 1); j++)
+		my_sumposition[j] = my_position[j] + my_sumposition[j - 1];
 
-	free(place), print_array(addplace, y + 1);
+	free(my_position), print_array(my_sumposition, y + 1);
 
 	my_sorted = malloc(sizeof(int) * size);
 	if (!my_sorted)
 		return;
-	start_initi(my_sorted, size);
+	my_arr_init(my_sorted, size);
 
-	for (sy = 0; sy < size; sy++)
+	for (sa = 0; sa < size; sa++)
 	{
-		addplace[array[sy]] -= 1;
-		my_sorted[addplace[array[sy]]] = array[sy];
+		my_sumposition[array[sa]] -= 1;
+		my_sorted[my_sumposition[array[sa]]] = array[sa];
 	}
-	free(addplace);
+	free(my_sumposition);
 
-	for (sy = 0; sy < size; sy++)
-		array[sy] = my_sorted[sy];
+	for (sa = 0; sa < size; sa++)
+		array[sa] = my_sorted[sa];
 	free(my_sorted);
 }
